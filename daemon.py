232b86678c64ac3e1051703215083d6f99dcbc73
leticiaDomingues <=>
2017-11-13 11:28:27 -0800

    Conexao por sockets entre o webserver e o daemon

--- daemon.py
+++ daemon.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python
 import cgitb
+import socket
+import sys
+
 cgitb.enable()
-print("Daemon!")
+
+#cria o socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#liga o socket com o host e a porta
+s.bind(("127.0.0.1",9001))
+
+#deixa o socket na espera de possiveis conexoes
+s.listen(1)
+
+while True:
+	print "[Daemon 1] Esperando conexao..."
+	conexao, endCliente = s.accept()
+
+	try:
+		print >>sys.stderr, "[Daemon 1] Conexao com o cliente: ", endCliente
+		while True:
+			dados = conexao.recv(4096)
+			print >>sys.stderr, "[Daemon1] Dados recebidos: %s " % dados
+			if dados:
+				print "[Daemon 1] Enviando dados de volta"
+				conexao.sendall("Leticia e Stephanie")
+			else:
+				print "[Daemon 1] Acabou os dados."
+				break
+	finally:
+		conexao.close()
+
+
+#TODO: outros daemons!!
+#TODO: executar os comandos
--- webserver.py
+++ webserver.py
@@ -11,7 +11,41 @@ print("Content-Type: text/html;charset=uft-8\r\n\r\n")
 dadosFormulario = cgi.FieldStorage()
 print dadosFormulario
 
-#cria o socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#address = ('192.168.56.101')
+#informacoes dos daemons
+hostDaemons = "127.0.0.1"
+portaDaemon1 = 9001
+portaDaemon2 = 9002
+portaDaemon3 = 9003
 
+#TODO: tratar dados do formulario
+
+#TODO: fazer pros outros daemons
+def encaminhaPacote():
+	#cria o socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	print "[Webserver] Socket do daemon 1 criado com sucesso."
+	
+	#faz a conexao com o daemon
+	s.connect((hostDaemons, portaDaemon1))
+	print "[Webserver] Conexao com o daemon 1 realizada." 
+
+	try:
+		#envia dados
+		msg = "Stephanie e Leticia"
+		print >>sys.stderr, "[Webserver] Enviando a msg: %s para Daemon 1" % msg
+		s.sendall(msg)
+	
+		#loop infinito
+		tamanhoEnviado = len(msg)
+		tamanhoRecebido = 0
+
+		while tamanhoRecebido < tamanhoEnviado:
+			dados = s.recv(4096)
+			tamanhoRecebido += len(dados)
+			print "[Webserver] Recebido: %s do daemon 1" % dados
+	finally:
+		s.close()
+		print "[Webserver] Socket do daemon 1 fechado."
+
+#TODO: criar as threads
+encaminhaPacote()

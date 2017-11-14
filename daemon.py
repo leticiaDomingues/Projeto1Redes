7797b19b25326cd92b3babc20ed9c2ce759c2bcf
leticiaDomingues <=>
2017-11-13 17:03:57 -0800

    Envio de pacote certo e execucao do comando no daemon

--- daemon.py
+++ daemon.py
@@ -2,14 +2,56 @@
 import cgitb
 import socket
 import sys
+import binascii
+import commands
 
 cgitb.enable()
 
+
+def executaComando(msg):
+	#pega o comando que o usuario quer
+	protocolBin = msg[72:80]
+	protocolo = ""
+	
+	if protocolBin=="00000001":
+		protocolo = "ps"
+	if protocolBin=="00000010":
+		protocolo = "df"
+	if protocolBin=="00000011":
+		protocolo = "finger"
+	if protocolBin=="00000100":
+		protocolo = "uptime"	
+
+	#pega as opcoes que o usuario digitou
+	optionsBin = msg[160:]
+	opcoes = binascii.unhexlify('%x' % int(optionsBin,2))
+	
+	#execute o comando
+	comando = protocolo + " " + opcoes
+	resultado = commands.getstatusoutput(comando)
+	
+	#monta o socket
+	msg = "0010" #version=2 (4 bits)
+	msg += "1111" #ihl (4 bits)
+	msg += "00000000" #type of service (8 bits)
+	msg += "0000000000000000" #total length (16 bits)
+	msg += "0101000001010000" #identification (16 bits)
+	msg += "111" #flags=111 (resposta) (3 bits)
+	msg += "0000000000000" #fragment offset = 0 (13 bits)
+	msg += "11111110" #time to live=254 (8 bits)
+	msg += protocolBin #protocol (8bits)
+	msg += "01111111" + "00000000" + "00000000" + "00000001" #source address
+	msg += "11000000" + "10101000" + "00111000" + "01100101" #destination
+	msg += str(resultado) #resposta
+
+	return msg[0:16]+bin(len(msg))[2:].zfill(16)+msg[32:len(msg)]
+
+	
 #cria o socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #liga o socket com o host e a porta
-s.bind(("127.0.0.1",9001))
+s.bind(("127.0.0.1",9006))
 
 #deixa o socket na espera de possiveis conexoes
 s.listen(1)
@@ -20,18 +62,17 @@ while True:
 
 	try:
 		print >>sys.stderr, "[Daemon 1] Conexao com o cliente: ", endCliente
-		while True:
-			dados = conexao.recv(4096)
-			print >>sys.stderr, "[Daemon1] Dados recebidos: %s " % dados
-			if dados:
-				print "[Daemon 1] Enviando dados de volta"
-				conexao.sendall("Leticia e Stephanie")
-			else:
-				print "[Daemon 1] Acabou os dados."
-				break
+		dados = conexao.recv(4096)
+		print >>sys.stderr, "[Daemon1] Dados recebidos: %s " % dados
+		msg = executaComando(dados)
+		if dados:
+			print "[Daemon 1] Enviando dados de volta"
+			conexao.sendall(msg)
+		else:
+			print "[Daemon 1] Acabou os dados."
+			break
 	finally:
 		conexao.close()
 
 
 #TODO: outros daemons!!
-#TODO: executar os comandos
--- webserver.py
+++ webserver.py
@@ -3,49 +3,63 @@ import cgi
 import cgitb
 import socket
 import sys
+import binascii
 
 cgitb.enable()
 print("Content-Type: text/html;charset=uft-8\r\n\r\n")
 
 #pega os dados que o cliente informou no formulario
 dadosFormulario = cgi.FieldStorage()
-print dadosFormulario
 
 #informacoes dos daemons
 hostDaemons = "127.0.0.1"
-portaDaemon1 = 9001
+portaDaemon1 = 9006
 portaDaemon2 = 9002
 portaDaemon3 = 9003
 
-#TODO: tratar dados do formulario
-
+#TODO: pegar dados do formulario
 #TODO: fazer pros outros daemons
+
 def encaminhaPacote():
 	#cria o socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print "[Webserver] Socket do daemon 1 criado com sucesso."
 	
 	#faz a conexao com o daemon
 	s.connect((hostDaemons, portaDaemon1))
-	print "[Webserver] Conexao com o daemon 1 realizada." 
 
 	try:
-		#envia dados
-		msg = "Stephanie e Leticia"
-		print >>sys.stderr, "[Webserver] Enviando a msg: %s para Daemon 1" % msg
+		#envia os dados
+		msg = criaMensagem()
 		s.sendall(msg)
-	
-		#loop infinito
-		tamanhoEnviado = len(msg)
-		tamanhoRecebido = 0
-
-		while tamanhoRecebido < tamanhoEnviado:
-			dados = s.recv(4096)
-			tamanhoRecebido += len(dados)
-			print "[Webserver] Recebido: %s do daemon 1" % dados
+		
+		#fica esperando a resposta
+		dados = s.recv(4096)
+		
+		#printa o resultado na tela
+		resultado = dados[160:]
+		print "Machine #1<br>"
+		resultado = resultado.replace("\n","<br>")
+		print resultado
 	finally:
 		s.close()
-		print "[Webserver] Socket do daemon 1 fechado."
+
+def criaMensagem():
+	#TODO: protocolo
+	msg = "0010" #version=2 (4 bits)
+	msg += "1111"#ihl(4 bits)
+	msg += "00000000" #type of service (8 bits)
+	msg += "0000000000000000" #total length (16 bits)
+	msg += "0000000000000000" #identification (16 bits)
+	msg += "000" #flags= 000 (requisicao) (3 bits)
+	msg += "0000000000000" # fragment offset = 0 (13 bits)
+	msg += "11111111" #time to live = 255 (8 bits)
+	msg += "00000001" #protocol =ps (8 bits)
+	msg += "0101010101010101"#header checksum (16bits)
+	msg += "11000000"+"10101000"+"00111000"+"01100101" #source address=192.168.56.101 (32 bits)
+	msg+= "01111111"+"00000000"+"00000000"+"00000001" #destination addres=127.0.0.1
+	msg+=bin(int(binascii.hexlify('-ef'),16))#options
+	return msg[0:16]+ bin(len(msg))[2:].zfill(16)+ msg[32:len(msg)]
+
 
 #TODO: criar as threads
 encaminhaPacote()
